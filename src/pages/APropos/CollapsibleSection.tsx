386691429a30520177c7d2c9468b5a6c3de610ea
Cuong Nguyen <cuong.shodan@gmail.com>
2024-09-13 23:47:09 +0200

    using framer-motion for smooth collapsing

--- src/pages/APropos/CollapsibleSection.tsx
+++ src/pages/APropos/CollapsibleSection.tsx
@@ -1,21 +1,24 @@
-// CollapsibleSection.tsx
 import React, { useState } from 'react';
 import { FaChevronUp, FaChevronDown } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
 
 interface CollapsibleSectionProps {
     title: string;
     children: React.ReactNode;
 }
 
-const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({ title, children }) => {
+const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({
+    title,
+    children,
+}) => {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleSection = () => {
-        setIsOpen((prevIsOpen) => !prevIsOpen);
+        setIsOpen(!isOpen);
     };
 
     return (
-        <div className={`collapsible-section${isOpen ? ' open' : ''}`}>
+        <div className="collapsible-section">
             <button
                 onClick={toggleSection}
                 className="collapsible-header"
@@ -24,18 +27,27 @@ const CollapsibleSection: React.FC<CollapsibleSectionProps> = ({ title, children
             >
                 <span>{title}</span>
                 {isOpen ? (
-                    <FaChevronDown className="arrow" />
-                ) : (
                     <FaChevronUp className="arrow" />
+                ) : (
+                    <FaChevronDown className="arrow" />
                 )}
             </button>
-            <div
-                id={`section-${title}`}
-                className="collapsible-content"
-                aria-hidden={!isOpen}
-            >
-                {children}
-            </div>
+            <AnimatePresence initial={false}>
+                {isOpen && (
+                    <motion.div
+                        id={`section-${title}`}
+                        className="collapsible-content"
+                        initial={{ height: 0, opacity: 0 }}
+                        animate={{ height: 'auto', opacity: 1 }}
+                        exit={{ height: 0, opacity: 0 }}
+                        transition={{ duration: 0.3, ease: 'easeInOut' }}
+                        style={{ overflow: 'hidden' }}
+                        aria-hidden={!isOpen}
+                    >
+                        {children}
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 };
